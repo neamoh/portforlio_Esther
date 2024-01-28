4a55d0c0c8961bb29ea3677deb719b1cdb128739
Esther Wairimu <63054708+neamoh@users.noreply.github.com>
2024-01-28 20:06:16 +0000

    Changes to be committed: 	modified:   src/App.jsx 	modified:   src/Components/About.jsx 	modified:   src/Components/Home.jsx 	modified:   src/Components/Portfolio.jsx 	new file:   src/images/background.jpg 	new file:   src/images/blackgirlcode.jpg 	new file:   src/images/server.jpg

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,14 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "pink abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description ="I'm a Software engineering student studying at Murang'a University of Technology. I am passionate about creating creative solutions to tech problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,10 +31,7 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Bash",
   "Graphic design",
 ];
 
@@ -45,7 +41,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I thrive on crafting innovative solutions that shape the future of cloud technology.";
 
 const About = () => {
   return (
